52a1e52fa18b7c6ea7288be8a19d769a5ff67310
Anirban Chetia <bloodraven166@gmail.com>
2019-10-06 09:36:33 +0530

    Possible word combos from phone digits.

--- Recursion/Phonedigit.java
+++ Recursion/Phonedigit.java
@@ -24,3 +24,24 @@ wordsutil(a,N,index+1,seq,curr+key[a[index]].charAt(seq+i));
 }
 
 }
+
+//OR:
+
+class phoneDigit{
+static void possibleWords(int a[], int N)
+{
+possibleWords(a,0,""); 
+}
+private static String keyHelper(int n){
+String str [] = {"","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+return str[n-1];
+}
+
+public static void possibleWords(int a[], int si, String outputSoFar){
+String temp = outputSoFar;
+if(si == a.length){
+System.out.print(outputSoFar + " ");
+return;
+}
+String str = keyHelper(a[si]);
+for(int i=0; i<str.length(); i++){="" outputsofar="outputSoFar" +="" str.charat(i);="" possiblewords(a,si+1,outputsofar);="" outputsofar="temp;" }="" }="" }="">
