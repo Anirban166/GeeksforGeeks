a5d264183bd9811a55f03f251a8f3e6e30f550d7
Anirban Chetia <bloodraven166@gmail.com>
2019-10-06 09:29:04 +0530

    Recursive solution for substring/subsets of a string.

---
+++ Recursion/Subset.cpp
@@ -0,0 +1,6 @@
+void printsub(string str, string curr=" ", int index=0)
+{ if(index==str.length())
+  { cout<<curr<<" "; return; }
+  printsub(str,curr,index+1);
+  printsub(str,curr+str[index],index+1);
+}
