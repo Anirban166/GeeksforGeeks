77da45fcf431088efb6431f5a83868fe63607d6c
Anirban Chetia <bloodraven166@gmail.com>
2019-10-12 00:52:55 +0530

    Height of Binary Tree.

---
+++ Tree/HeightBT.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+/* Computes the number of nodes in a tree. */
+int height(struct Node* node);
+void inorder(Node *root)
+{
+    if (root == NULL)
+       return;
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);    
+}
+/* Driver program to test size function*/
+int main()
+{
+  int t;
+  scanf("%d
+", &t);
+  while (t--)
+  {
+     map<int, Node*> m;
+     int n;
+     scanf("%d",&n);
+     struct Node *root = NULL;
+     struct Node *child;
+     while (n--)
+     {
+        Node *parent;
+        char lr;
+        int n1, n2;
+        scanf("%d %d %c", &n1, &n2, &lr);
+        if (m.find(n1) == m.end())
+        {
+           parent = new Node(n1);
+           m[n1] = parent;
+           if (root == NULL)
+             root = parent;
+        }
+        else
+           parent = m[n1];
+        child = new Node(n2);
+        if (lr == 'L')
+          parent->left = child;
+        else
+          parent->right = child;
+        m[n2]  = child;
+     }
+     cout << height(root) << endl;
+  }
+  return 0;
+}
+
+/* Tree node structure  used in the program
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+/* Computes the height of binary tree with given root.  */
+int height(Node* node)
+{
+if(!node)
+return 0;
+int h=0;
+queue<Node*> q;
+q.push(node);
+while(!q.empty())
+{
+int size = q.size();
+while(size)
+{
+Node* cur = q.front();
+q.pop();
+if(cur->left)
+q.push(cur->left);
+if(cur->right)
+q.push(cur->right);
+size--;
+}
+h++;
+}
+return h;
+// Your code here
+}
