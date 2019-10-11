3f8d4431cfd9c5c50de578f0cf447d8d58a73231
Anirban Chetia <bloodraven166@gmail.com>
2019-10-11 14:30:48 +0530

    Queue using Stacks.

---
+++ Queue/QueueStack.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+using namespace std;
+class StackQueue{
+private:
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int B);
+    int pop();
+};
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        StackQueue *sq = new StackQueue();
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+        }
+        }
+        cout<<endl;
+    }
+}
+
+
+class StackQueue
+{
+
+private:   
+    // These are STL stacks ( http://goo.gl/LxlRZQ )
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int);
+    int pop();
+}; 
+
+void StackQueue :: push(int x)
+ {
+        s1.push(x);
+        
+ }
+int StackQueue :: pop()
+{
+ 
+        if(s2.empty())
+        {
+            if(s1.empty())
+            {
+                return -1;
+            }
+            else{
+                while(!s1.empty())
+                {
+                    int ele=s1.top();
+                    s1.pop();
+                    s2.push(ele);
+                }
+                int ele=s2.top();
+                s2.pop();
+                return ele;
+            }
+        }
+        // S2 is not empty so just pop out element from it
+        else
+        {
+            int ele=s2.top();
+            s2.pop();
+            return ele;
+        }
+}
